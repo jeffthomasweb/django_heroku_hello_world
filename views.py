fe5538cf17ea99d8ee897fc807caa4746cbdd626
jeffthomasweb <93561509+jeffthomasweb@users.noreply.github.com>
2021-11-04 05:19:14 +0000

    Delete views.py

--- views.py
+++
@@ -1,7 +0,0 @@
-
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("<h1 style='font-family: Arial; padding: 10%; text-align: center; font-size 4em'> Hello RocPy!🪨 🐍</h1>")
